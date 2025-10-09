2ef1432503c2d91f09d45babbcef77e0333f0f28
Rahu1007 <rahulssharma75247@gmail.com>
2025-10-09 16:53:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const App = () => {
+  return (
+  <div className="h-screen w-screen bg-red-100">
+    <nav>
+      <a href="/create ">ADD New Product</a>
+      <h1>Category</h1>
+      <ul>
+        <li>cat1</li>
+      </ul>
+    </nav>
+
+  </div>
+  );
+}
+export default App;
